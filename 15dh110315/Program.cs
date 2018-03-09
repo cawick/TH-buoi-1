3f252fc5de308a54fd443aa316781c00ad57a425
cawick <th3lastphant0m@gmail.com>
2018-03-09 14:23:45 +0700

    baitap1

---
+++ 15dh110315/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15dh110315
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("awdasdawdsadawd");
+        }
+    }
+}
